26ce14beddca494f371a39807c0b1d6a3eb2653f
Joe Shmo <joe.burge.iii@gmail.com>
2024-02-27 22:09:39 -0800

    updated linting

--- gaslighter/fluids/__init__.py
+++ gaslighter/fluids/__init__.py
@@ -2,7 +2,7 @@ from .basic_volume import *
 from .ideal_gas import *
 from .incompressible import *
 from .intensive_state import *
+from .orifice import IncompressibleOrifice
+from .pipe import IncompressiblePipe
 from .real_gas import *
 from .rocket_chamber import *
-from .pipe import IncompressiblePipe
-from .orifice import IncompressibleOrifice  
