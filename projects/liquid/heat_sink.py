80ba207f77f91fb90f65f33b428be97f0b2b3075
Joe Wilson <your_email@example.com>
2024-03-10 12:01:00 -0700

    results good enough for now

--- projects/liquid/heat_sink.py
+++ projects/liquid/heat_sink.py
@@ -1,62 +1,46 @@
+import numpy as np
+
+from gaslighter.heat_transfer import plot_fdm_solution
+from gaslighter import STD_ATM_K, convert
 
 # Heat Sink Graph / Model of the engine
+
 # Materials from : https://www.pcbway.com/rapid-prototyping/cnc-machining/metal/copper/Copper-0/
 
-import gaslighter_rust
-from gaslighter import PCBWAY_CU_DIFFUSIVITY, STD_ATM_K, convert
-import numpy as np
-import plotly.graph_objects as go
-
-"""
-I don't love this scrapy glueing of rust and python,
-but I don't want to spend lots of time making nice looking iterfaces
-"""
-
-def plot_results(results: list):
-    d = np.zeros_like(results[:][-1])
-    for i, dx in enumerate(results[:][-1]):
-
-        if i == 0:
-            continue
-
-        d[i] = d[i - 1] + dx
+CONVECTION_TEMP_THROAT_K = 1446.17034373
+NODES = 250
+MAX_THICKNESS_IN = convert(3, 'in', 'm')
+MAX_TIME = 5
 
 
-    time = results[:][-2]
-    temp_history = results[:][0:-1]
-
-    # Create initial plot
-    fig = go.Figure()
-
-    # Add initial trace (only the first temperature vs distance graph)
-    fig.add_trace(go.Heatmap(z=temp_history[0], y=[time[0]] * len(d), x=d, colorscale='Viridis'))
-
-    # Add scrollbar
-    fig.update_layout(
-        title="Temperature vs Distance",
-        xaxis_title="Distance [m]",
-        yaxis_title="Time [s]",
-        sliders=[{
-            "active": 0,
-            "steps": [{"label": f"Time: {t}", "method": "update", "args": [{"z": [temp_history[i]]}, {"title": f"Temperature vs Distance - Time: {t}"}]} for i, t in enumerate(time)],
-            "pad": {"t": 50},
-            "currentvalue": {"visible": True, "prefix": "Time: "},
-            "transition": {"duration": 300, "easing": "cubic-in-out"}
-        }]
-    )
-
-    fig.show()
-
-intial_temps = list(np.zeros(100))
-intial_temps[0] = 100
-
-results = gaslighter_rust.fdm_1d(
-    intial_temps,
-    0.001,
-    1,
+# Create a polynomial to find average diffusivity of our system
+# https://www.researchgate.net/figure/Thermal-diffusivity-of-copper-as-a-functlon-of-temperature_fig1_236269958
+copper_high_temp_diffivity = np.poly1d(np.polyfit(
+    convert([614.6031746031746,718.7301587301588,843.1746031746032,934.6031746031746,1008.2539682539683], 'degC', 'degK'),
+    convert([0.4677551020408163,0.3648979591836734,0.23755102040816323,0.14693877551020407,0.07836734693877556], 'cm^2/s', 'm^2/s'),
     1
+))
+AVG_DIFFUSIVITY = copper_high_temp_diffivity(np.mean([CONVECTION_TEMP_THROAT_K, STD_ATM_K]))
+
+# NOTE: There is a serious limitation of this fdm solve,
+# it does not consider convection, and assumes the outter wall will be ambient... which it won't be
+plot_fdm_solution(
+    diffusivity = AVG_DIFFUSIVITY,
+    start_t = CONVECTION_TEMP_THROAT_K,
+    end_t = STD_ATM_K,
+    nodes = NODES,
+    distance = MAX_THICKNESS_IN,
+    max_time = MAX_TIME,
+    export_path = "plots/heat_imperial.html",
+    imperial_results=True
 )
-
-plot_results(results)
-
-
+plot_fdm_solution(
+    diffusivity = AVG_DIFFUSIVITY,
+    start_t = CONVECTION_TEMP_THROAT_K,
+    end_t = STD_ATM_K,
+    nodes = NODES,
+    distance = MAX_THICKNESS_IN,
+    max_time = MAX_TIME,
+    export_path = "plots/heat_sink.html",
+    imperial_results=False
+)
